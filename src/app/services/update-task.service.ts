daee469b9f4f9254f72663db205850bb68b6714b
dhrithi <dhriti280@gmail.com>
2023-11-29 10:19:16 -0800

    updated service to handle get task by id api

--- src/app/services/update-task.service.ts
+++ src/app/services/update-task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -7,9 +8,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UpdateTaskService {
   private apiUrl = 'http://127.0.0.1:5000/api/updateTask/';
+  private getTaskByIdUrl = 'http://127.0.0.1:5000/api/tasks/';
 
 
   constructor(private http: HttpClient) { }
+
+  getTask(taskId: string): Observable<any> {
+    return this.http.get(`${this.getTaskByIdUrl}/tasks/${taskId}`);
+
+  }
 
   updateTask(taskId: string, taskData: any) {
     return this.http.put(`${this.apiUrl}${taskId}`, taskData);
